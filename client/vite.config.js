e5c07edaf8e484c109dd819e20999deca36b4900
LakshmiArul <muthu.lakshmi980@gmail.com>
2026-09-24 20:43:44 +0530

    live url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://mini-wms-server-ih58.onrender.com'
     }
   }
 });
