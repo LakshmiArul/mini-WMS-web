51444de4a16c99d06ce0b752414f3efa9621ad0b
LakshmiArul <muthu.lakshmi980@gmail.com>
2026-09-24 20:58:47 +0530

    CORs error

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,22 @@ const { inventorySummary, addInward, createOrder, checkFeasibility, dispatchOrde
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = new Set([
+  'https://mini-wms-web.vercel.app',
+  'http://localhost:5173',
+]);
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.has(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 
 app.use(express.json());
 
